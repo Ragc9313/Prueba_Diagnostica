d1ed683d378d3f84164e5da477946df983264d2f
Ragc9313 <alito7ragc13@gmail.com>
2024-03-05 14:11:10 -0600

    prueba6

--- bienvenida.php
+++ bienvenida.php
@@ -6,54 +6,75 @@
 
     <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet">
     
-    <link rel="stylesheet" href="login/css/estilos.css">
+    <link rel="stylesheet" href="login/css/estilos2.css">
+
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+    integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZwv-model-vue1T"
+    crossorigin="anonymous"/>
+
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet">
+
+
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" 
+        integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
+        crossorigin="anonymous">
+    </script>
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"
+    integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1"
+    crossorigin="anonymous">
+    </script>
+
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"
+    integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM"
+    crossorigin="anonymous"> 
+    </script>
+
+    <link rel="stylesheet" href="login/css/estilos2.css">
 
 </head>
 <body>
-    <h1>Honduventas</h1>    
+    <h1 class="titulo">Honduventas</h1>    
 
     <div class="contenedor_principal">
 
-        <div class="contenedor_izquierda">
-            <button>Inicio</button>
-            <button>Empleados</button>
-            <button>Usuarios</button>
-            <button>Roles</button>
-            <button>Cerrar Sesión</button>
-        </div>
-        <div class="contenedor_derecha">
-            <div class="contenedor_usuarios">
+        <div class="row">
+            <div class= "column 1">
+                <ul class="colum_list">
+                    <ul class=" lista"> <button style="margin: 0.5em auto" type="submit " class="send-btn">Inicio</button> </ul>
+                    <ul class=" lista"> <button style="margin:0.5em auto" type="submit " class="send-btn">Empleados</button> </ul>
+                    <ul class=" lista"><button style="margin: 0.5em auto" type="submit " class="send-btn">Usuarios</button> </ul>
+                    <ul class=" lista">  <button style="margin: 0.5em auto" type="submit " class="send-btn">Roles</button> </ul>
+                    <ul class=" lista">  <button style="margin: 0.5em auto" type="submit" class="send-btn">Cancelar</button> </ul>
+                </ul>
+
+            <div class = "col col-6">
+                <div class="contenedor_usuarios">
+                    <div class="contenedor_imagen">
+                        <img></img>
+                    </div>
+                </div>
+
+                <div class="contenedor_empleados">
                 <div class="contenedor_imagen">
                     <img></img>
+                </div>
+
+                <div class="contenedor_roles">
+                <div class="contenedor_imagen">
+                    <button></button>
+                </div>
+
+                <div class="contenedor_reportes">
+                <div class="contenedor_imagen">
                     <button></button>
                 </div>
             </div>
-
-            <div class="contenedor_Empleados">
-            <div class="contenedor_imagen">
-                <img></img>
-                <button></button>
-            </div>
-
-            <div class="contenedor_roles">
-            <div class="contenedor_imagen">
-                <img></img>
-                <button></button>
-            </div>
-
-            <div class="contenedor_reportes">
-            <div class="contenedor_imagen">
-                <img></img>
-                <button></button>
-            </div>
              
         </div>
-        </div>
-
+         </div>
         </div>
     </div>
     
 </body>
 </html>
-
-
